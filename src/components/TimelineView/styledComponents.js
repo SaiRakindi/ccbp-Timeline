927c30c793724c0a1b1157d974bce52bc2365131
SaiRakindi <sai.rakindi3@gmail.com>
2021-09-18 09:14:10 +0000

    first commit

--- src/components/TimelineView/styledComponents.js
+++ src/components/TimelineView/styledComponents.js
@@ -4,7 +4,7 @@ import styled from 'styled-components/macro'
 
 export const TimelineContainer = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   min-height: 100vh;
 `
@@ -14,7 +14,7 @@ export const ResponsiveContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-width: 1110px;
+  max-width: 1110px;
   height: 100vh;
 `
 
@@ -27,13 +27,24 @@ export const HeaderContainer = styled.div`
 
 export const Heading = styled.h1`
   text-align: center;
+  color: #171f46;
   font-family: 'Roboto';
   font-weight: 500;
   font-size: 18px;
-  color: #171f46;
+  line-height: 2.7;
+  margin: 0px;
   @media screen and (min-width: 768px) {
     font-size: 20px;
   }
 `
 
-export const CCBPHeading = styled.span``
+export const CCBPHeading = styled.span`
+  color: #2b237c;
+  font-family: 'Roboto';
+  font-weight: 500;
+  font-size: 24px;
+  margin: 0px;
+  @media screen and (min-width: 768px) {
+    font-size: 30px;
+  }
+`
